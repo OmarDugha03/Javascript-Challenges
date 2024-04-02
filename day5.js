c7909d71561c422e4c3c03beb5f7a729d62f90af
OmarDugha <116832203+OmarDugha03@users.noreply.github.com>
2024-04-02 15:40:49 +0300

    Chunk function

---
+++ day5.js
@@ -0,0 +1,11 @@
+function chunk(arr, size) {
+  const result = []
+  let index = 0
+  while (index < arr.length) {
+    result.push(arr.slice(index, index + size))
+    index += size
+  }
+  return result
+}
+
+console.log(chunk([1, 2, 3, 4], 2))
